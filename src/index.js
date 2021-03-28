f30ee9ef5ff8ed07a6fa4fef82cdaa1a1b3c0b15
olivares-andres <andresolivares1986@gmail.com>
2021-03-27 22:05:18 -0300

    corrigiendo errores

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors')
+const bodyParser = require('body-parser')
 
 // Settings
 app.set('port', process.env.PORT || 3001);
@@ -8,6 +9,7 @@ app.set('port', process.env.PORT || 3001);
 // Middleware
 app.use(cors())
 app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}))
 
 // Routes
 app.use(require('./routes/store'));
